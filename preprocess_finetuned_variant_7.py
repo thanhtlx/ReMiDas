7817a7677599f66fd74464f2d2aa4a590afe7d07
thanhtlx <thanh1908.xx@gmail.com>
2023-06-13 09:58:14 +0700

    tmp

--- preprocess_finetuned_variant_7.py
+++ preprocess_finetuned_variant_7.py
@@ -1,3 +1,4 @@
+import pathlib
 from transformers import RobertaTokenizer, RobertaModel
 import pandas as pd
 import json
@@ -16,9 +17,8 @@ FINE_TUNED_MODEL_PATH = 'model/patch_variant_7_finetuned_model.sav'
 
 dataset_name = 'ase_dataset_sept_19_2021.csv'
 # dataset_name = 'huawei_sub_dataset.csv'
-import pathlib
-dataset_name ='big_vf.csv'
-dataset_name ='test.csv'
+dataset_name = 'big_vf.csv'
+dataset_name = 'test.csv'
 CODE_LINE_LENGTH = 128
 
 use_cuda = cuda.is_available()
@@ -31,7 +31,8 @@ torch.backends.cudnn.benchmark = True
 
 
 def get_input_and_mask(tokenizer, code_list):
-    inputs = tokenizer(code_list, padding='max_length', max_length=CODE_LINE_LENGTH, truncation=True, return_tensors="pt")
+    inputs = tokenizer(code_list, padding='max_length',
+                       max_length=CODE_LINE_LENGTH, truncation=True, return_tensors="pt")
 
     return inputs.data['input_ids'], inputs.data['attention_mask']
 
@@ -63,7 +64,8 @@ def get_hunk_embeddings(code_list, tokenizer, code_bert):
     with torch.no_grad():
         input_ids = input_ids.to(device)
         attention_mask = attention_mask.to(device)
-        embeddings = code_bert(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state[:, 0, :]
+        embeddings = code_bert(
+            input_ids=input_ids, attention_mask=attention_mask).last_hidden_state[:, 0, :]
     embeddings = embeddings.tolist()
     return embeddings
 
@@ -98,8 +100,9 @@ def write_embeddings_to_files(removed_embeddings, added_embeddings, removed_url_
     for url, data in url_to_data.items():
         file_path = os.path.join(directory, EMBEDDING_DIRECTORY)
         pathlib.Path(file_path).mkdir(parents=True, exist_ok=True)
-        file_path =  file_path + '/' + url.replace('/', '_') + '.txt'
+        file_path = file_path + '/' + url.replace('/', '_') + '.txt'
         json.dump(data, open(file_path, 'w'))
+
 
 def hunk_empty(hunk):
     if hunk.strip() == '':
@@ -139,7 +142,7 @@ def get_data():
         model = nn.DataParallel(model)
 
     model.load_state_dict(torch.load(FINE_TUNED_MODEL_PATH))
-    #change
+    # change
     code_bert = model.code_bert
 
     if torch.cuda.device_count() > 1:
@@ -152,13 +155,17 @@ def get_data():
 
     print("Reading dataset...")
     df = pd.read_csv(dataset_name)
-    df = df[['commit_id', 'repo', 'partition', 'diff', 'label', 'PL', 'LOC_MOD', 'filename']]
+    df = df[['commit_id', 'repo', 'partition',
+             'diff', 'label', 'PL', 'LOC_MOD', 'filename']]
+    print(df.shape)
     items = df.to_numpy().tolist()
 
     url_to_hunk = {}
 
     for item in items:
         commit_id = item[0]
+        if commit_id == "b12d92efd6c0d48665383a9baecc13e7ebbd8a22":
+            print(item)
         repo = item[1]
         url = repo + '/commit/' + commit_id
         diff = item[3]
@@ -173,7 +180,8 @@ def get_data():
     removed_url_list = []
     added_url_list = []
     for url, diff_list in tqdm.tqdm(url_to_hunk.items()):
-        file_path = os.path.join(directory, EMBEDDING_DIRECTORY + '/' + url.replace('/', '_') + '.txt')
+        file_path = os.path.join(
+            directory, EMBEDDING_DIRECTORY + '/' + url.replace('/', '_') + '.txt')
         if os.path.isfile(file_path):
             continue
 
@@ -189,10 +197,13 @@ def get_data():
                 added_url_list.append(url)
 
         if len(removed_code_list) >= 50 or len(added_code_list) >= 50:
-            removed_embeddings = get_hunk_embeddings(removed_code_list, tokenizer, code_bert)
-            added_embeddings = get_hunk_embeddings(added_code_list, tokenizer, code_bert)
+            removed_embeddings = get_hunk_embeddings(
+                removed_code_list, tokenizer, code_bert)
+            added_embeddings = get_hunk_embeddings(
+                added_code_list, tokenizer, code_bert)
 
-            write_embeddings_to_files(removed_embeddings, added_embeddings, removed_url_list, added_url_list)
+            write_embeddings_to_files(
+                removed_embeddings, added_embeddings, removed_url_list, added_url_list)
 
             removed_code_list = []
             added_code_list = []
@@ -200,9 +211,12 @@ def get_data():
             added_url_list = []
 
     if len(removed_code_list) > 0 or len(added_code_list) > 0:
-        removed_embeddings = get_hunk_embeddings(removed_code_list, tokenizer, code_bert)
-        added_embeddings = get_hunk_embeddings(added_code_list, tokenizer, code_bert)
-        write_embeddings_to_files(removed_embeddings, added_embeddings, removed_url_list, added_url_list)
+        removed_embeddings = get_hunk_embeddings(
+            removed_code_list, tokenizer, code_bert)
+        added_embeddings = get_hunk_embeddings(
+            added_code_list, tokenizer, code_bert)
+        write_embeddings_to_files(
+            removed_embeddings, added_embeddings, removed_url_list, added_url_list)
 
 
 if __name__ == '__main__':
