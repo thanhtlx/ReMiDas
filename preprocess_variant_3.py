b055c787ce3980b34d2f25f186bb9570e78c3a09
thanhtlx <thanh1908.xx@gmail.com>
2023-06-23 11:01:35 +0700

    tmp

--- preprocess_variant_3.py
+++ preprocess_variant_3.py
@@ -28,7 +28,8 @@ torch.backends.cudnn.benchmark = True
 
 
 def get_input_and_mask(tokenizer, code_list):
-    inputs = tokenizer(code_list, padding=True, max_length=CODE_LINE_LENGTH, truncation=True, return_tensors="pt")
+    inputs = tokenizer(code_list, padding=True, max_length=CODE_LINE_LENGTH,
+                       truncation=True, return_tensors="pt")
 
     return inputs.data['input_ids'], inputs.data['attention_mask']
 
@@ -36,7 +37,7 @@ def get_input_and_mask(tokenizer, code_list):
 def get_code_version(diff, added_version):
     code = ''
     lines = diff.splitlines()
-    for line in lines:
+    for line in lines[:150]:
         if len(line.split()) > 50:
             continue
         mark = '+'
@@ -58,7 +59,8 @@ def get_hunk_embeddings(code_list, tokenizer, code_bert):
     with torch.no_grad():
         input_ids = input_ids.to(device)
         attention_mask = attention_mask.to(device)
-        embeddings = code_bert(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state[:, 0, :]
+        embeddings = code_bert(
+            input_ids=input_ids, attention_mask=attention_mask).last_hidden_state[:, 0, :]
         input_ids.to('cpu')
         attention_mask.to('cpu')
         embeddings.to('cpu')
@@ -79,7 +81,8 @@ def write_embeddings_to_files(code_list, url_list, tokenizer, code_bert):
         data = {'embeddings': embeddings}
         url_to_data[url] = data
     for url, data in url_to_data.items():
-        file_path = os.path.join(directory, EMBEDDING_DIRECTORY + '/' + url.replace('/', '_') + '.txt')
+        file_path = os.path.join(
+            directory, EMBEDDING_DIRECTORY + '/' + url.replace('/', '_') + '.txt')
         json.dump(data, open(file_path, 'w'))
 
 
@@ -97,7 +100,7 @@ def hunk_empty(hunk):
 def get_hunk_from_diff(diff):
     hunk_list = []
     hunk = ''
-    for line in diff.split('\n'):
+    for line in diff.split('\n')[:150]:
         if line.startswith(('+', '-')):
             hunk = hunk + line + '\n'
         else:
@@ -114,7 +117,8 @@ def get_hunk_from_diff(diff):
 
 def get_data():
     tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
-    code_bert = RobertaModel.from_pretrained("microsoft/codebert-base", num_labels=2)
+    code_bert = RobertaModel.from_pretrained(
+        "microsoft/codebert-base", num_labels=2)
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
@@ -124,7 +128,8 @@ def get_data():
     code_bert.eval()
     print("Reading dataset...")
     df = pd.read_csv(dataset_name)
-    df = df[['commit_id', 'repo', 'partition', 'diff', 'label', 'PL', 'LOC_MOD', 'filename']]
+    df = df[['commit_id', 'repo', 'partition',
+             'diff', 'label', 'PL', 'LOC_MOD', 'filename']]
     items = df.to_numpy().tolist()
 
     url_to_hunk = {}
@@ -143,7 +148,8 @@ def get_data():
     code_list = []
     url_list = []
     for url, diff_list in tqdm.tqdm(url_to_hunk.items()):
-        file_path = os.path.join(directory, EMBEDDING_DIRECTORY + '/' + url.replace('/', '_') + '.txt')
+        file_path = os.path.join(
+            directory, EMBEDDING_DIRECTORY + '/' + url.replace('/', '_') + '.txt')
         if os.path.isfile(file_path):
             continue
         for i, diff in enumerate(diff_list):
@@ -156,7 +162,8 @@ def get_data():
             url_list.append(url)
 
         if len(url_list) >= 100:
-            write_embeddings_to_files(code_list, url_list, tokenizer, code_bert)
+            write_embeddings_to_files(
+                code_list, url_list, tokenizer, code_bert)
             code_list = []
             url_list = []
 
